8f444a958be005dc32eaf3d72e8eec5b178b3270
Shuntaro-333 <24fi019@ms.dendai.ac.jp>
2026-06-12 12:16:57 +0900

    mainUp

--- main.py
+++ main.py
@@ -1,33 +1,15 @@
-from typing import Optional
-
 from fastapi import FastAPI
-
-import random  # randomライブラリを追加
+import random
 
 app = FastAPI()
 
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def read_root():
+    return {"Hello": "World"}
 
 @app.get("/omikuji")
 def omikuji():
-    omikuji_list = [
-        "大吉",
-        "中吉",
-        "小吉",
-        "吉",
-        "半吉",
-        "末吉",
-        "末小吉",
-        "凶",
-        "小凶",
-        "大凶"
-    ]
-
-    return omikuji_list[random.randrange(10)]
+    result = random.choice(
+        ["大吉", "中吉", "小吉", "吉", "末吉", "凶"]
+    )
+    return {"result": result}
