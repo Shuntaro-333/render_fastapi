bfc4f4e61f774033442aa27db7aa29094d431a1c
Shuntaro-333 <24fi019@ms.dendai.ac.jp>
2026-06-18 13:45:05 +0900

    main_updateKadai

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 import random
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+### コードいろいろ... ###
+# 課題１
 
-@app.get("/omikuji")
-def omikuji():
-    result = random.choice(
-        ["大吉", "中吉", "小吉", "吉", "末吉", "凶"]
-    )
-    return {"result": result}
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>ホームページ</title>
+        </head>
+        <body>
+            <h1>ようこそ！</h1>
+            <p>これは、24FI019のホームページです。</p>
+
+            <h2>趣味</h2>
+            <ul>
+            <li>卓球</li>
+            <li>ゲーム(FPS)</li>
+            </ul>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+#課題２
+@app.post("/present")
+async def give_present(present: str):
+
+    gifts = [
+        "キャンディー",
+        "ケーキ",
+        "ぬいぐるみ",
+        "ゲームソフト",
+        "チョコレート",
+        "図書カード"
+    ]
+
+    return_gift = random.choice(gifts)
+    return {
+        "response": f"{present}をありがとう！お返しに{return_gift}をプレゼントします！"
+    }
